942a08022774e5c4809ca1e5a77a53b405fd12b0
Gavrichenko Kate <gavrichenko.eo@phystech.edu>
2021-10-02 11:55:02 +0300

    Modified testFile.py to dispay a nice structure

--- testFile.py
+++ testFile.py
@@ -1,1 +1,11 @@
-print("Hello, MIPT!)
+import turtle  as t
+
+colors = ["red", "purple", "blue", "green", "orange", "yellow"]
+pen = t.Pen()
+t.bgcolor('black')
+for x in range(360):
+
+    pen.pencolor(colors[x%6])
+    pen.width(x//100 + 1)
+    pen.forward(x)
+    pen.left(59)
